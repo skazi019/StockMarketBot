f01acef8306dcf6cdd5963d33e1da828117d360b
Kaushal Sharma <skazi019@gmail.com>
2021-11-05 23:20:59 +0530

    Debugging fetch_data.py

--- Code/Core/fetch_data.py
+++ Code/Core/fetch_data.py
@@ -6,9 +6,10 @@ This file fetches data for the specified scrip from the exchange
 
 from Code.Utilities.config.config_util import ConfigUtil
 
+import aiohttp
+
 
 class FetchData:
-
     __config = ConfigUtil._get_config('stockmarket')
     __querystring = {"datatype": "json"}
 
@@ -18,9 +19,17 @@ class FetchData:
     }
 
     @classmethod
-    def _fetch_data(cls, scrip='BANKNIFTY', timeframe='5m'):
+    def _fetch_data(cls, symbol='BANKNIFTY', timeperiod='TIME_SERIES_INTRADAY', interval='5m'):
+        cls.__querystring['function'] = timeperiod
+        cls.__querystring['symbol'] = symbol
+        cls.__querystring['interval'] = interval
+        with aiohttp.ClientSession() as session:
+            with session.get(cls.__config['alphavantage']['url'],
+                             headers=cls.__headers, params=cls.__querystring) as data:
+                return data
 
-        pass
 
-
-FetchData()
+try:
+    data = FetchData._fetch_data(symbol='MSFT', timeperiod='TIME_SERIES_INTRADAY', interval='5m')
+except Exception as e:
+    print(e)
