b31b3e7a6c57e5aee8cf744eb1121e680d7d2547
Kaushal Sharma <Kaushal.Sharma2@in.ey.com>
2022-01-11 15:40:26 +0530

    near 200 ema minor fix

--- Code/Algorithms/near_ma.py
+++ Code/Algorithms/near_ma.py
@@ -11,8 +11,8 @@ class NearMA:
     async def near_200_ema(ticker_df):
         ema = ticker_df.iloc[-1]['200_EMA']
         close = ticker_df.iloc[-1]['Close']
-        upper_limit = ema + ema * ConfigUtil.get_config(server='algorithms')['near_200_ema']
-        lower_limit = ema - ema * ConfigUtil.get_config(server='algorithms')['near_200_ema']
+        upper_limit = ema + (ema * ConfigUtil.get_config(server='algorithms')['near_200_ema'])
+        lower_limit = ema - (ema * ConfigUtil.get_config(server='algorithms')['near_200_ema'])
         if lower_limit <= close <= upper_limit:
             return True, close
         else:
